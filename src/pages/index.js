ca4edc477912f1b7b58d5c7885117d0d13d3b3fe
DrewPhamPowerschool <drew.pham@hobsons.com>
2022-08-04 21:00:54 +0100

    orange test

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,12 +50,166 @@ const IndexPage = () => {
       <Seo description={data.allDescriptionsJson.nodes[0].description} slug='' category={data.allDescriptionsJson.nodes[0].page_location} />
 
         <div className="container-fluid p-0">
-          <div className="row single-post m-auto justify-content-between mt-2 mt-lg-5">
-            {data.allBlogDataJson.edges.map((data, index) => {
+          <div className="single-post">
+
+
+
+
+
+          // first block    
+            <div className="row mt-2 mt-lg-5 me-0">
+              <div className="col-8">
+                <div className="row">
+
+
+                  {data.allBlogDataJson.edges.slice(0,5).map((data, index) => {
+                    var excerpto = data.node.excerpt.substring(0,100).split(' ')
+                    
+                    return (    
+                      <>
+                      <Link to={`/${data.node.slug}/`} className='linkcard'>
+                      <div className="row pb-5" key={`content_item_${index}`}>
+                        <div className="col-3">
+                          <img class="" src={data.node.IMAGE_URL}  width="100%" height="100%" alt="Card image cap"/>
+
+                        </div>
+                        <div className="col-8">
+
+                            <div className="card-body ps-1 sec-color py-2">
+                            <h2 className="card-big-title">{data.node.H1}</h2>
+                            <p className="card-text" id="cardo">{excerpto.slice(0, excerpto.length - 1).join(' ')} ...</p>
+                            
+                            <div  className="card-link">Read More →</div>
+                            
+                          
+                          </div>
+                        </div>
+                      </div>
+                      </Link>
+                      </>
+                    );
+                  })}
+
+
+                </div>
+              </div>
+              <div className="col-4 ps-5">
+                <div className="row">
+
+
+                  {data.allBlogDataJson.edges.slice(0,2).map((data, index) => {
+                    var excerpto = data.node.excerpt.substring(0,100).split(' ')
+                    
+                    return (    
+                      <>
+                      <div className="col-12 p-0" key={`content_item_${index}`}>
+                        <img class="card-img-top" src={data.node.IMAGE_URL} alt="Card image cap"/>
+
+                        <div className="card-body mb-3 sec-color bg-white">
+                        <Link to={`/${data.node.slug}/`} className='linkcard'>
+
+                          <h2 className="card-title">{data.node.H1}</h2>
+                          <p className="card-text">{excerpto.slice(0, excerpto.length - 1).join(' ')} ...</p>
+                          
+                          </Link>
+                        </div>
+                      </div>
+                      </>
+                    );
+                  })}
+
+                  
+                </div>
+              </div>
+            </div>
+
+
+          
+
+
+          //second block
+          <div className="row mt-2 mt-lg-5 me-0">
+            <div className="col-12">
+              <h2 className="cat-small-title mb-5">Category Name</h2>
+              <div className="row justify-content-between">
+
+
+
+
+
+                  {data.allBlogDataJson.edges.slice(0,4).map((data, index) => {
+                        var excerpto = data.node.excerpt.substring(0,75).split(' ')
+                        
+                        return (    
+                          <>
+                          <div className="col-6" style={{width:"48%"}}>
+
+                            <Link to={`/${data.node.slug}/`} className='linkcard'>
+                              <div className="row pb-4" key={`content_item_${index}`}>
+                                <div className="col-4">
+                                  <img class="" src={data.node.IMAGE_URL} width="100%" height="100%" alt="Card image cap"/>
+
+                                </div>
+                                <div className="col-8 px-0">
+
+                                    <div className="card-body py-0 sec-color">
+                                    <h2 className="card-title">{data.node.H1}</h2>
+                                    <p className="card-text">{excerpto.slice(0, excerpto.length - 1).join(' ')} ...</p>
+                                    
+                                    
+                                  
+                                  </div>
+                                </div>
+                              </div>
+                            </Link>
+                          </div>
+                          </>
+                        );
+                      })}
+
+
+
+
+
+
+                   
+
+              </div>
+            </div>
+          </div>
+
+
+
+          //third block
+
+
+
+
+
+
+
+
+
+
+          <div className="row mt-2 mt-lg-5 me-0">
+
+
+              <div className="col-4">
+                <div className="row">
+
+<ins class="adsbygoogle"
+     style={{display:"block"}}
+     data-ad-client="ca-pub-9808253958086114"
+     data-ad-slot="5972256991"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+
+
+                {data.allBlogDataJson.edges.slice(0,3).map((data, index) => {
               var excerpto = data.node.excerpt.substring(0,100).split(' ')
               
               return (    
-                <div className="col-12 col-md-6 col-lg-4 p-0 card-holder" key={`content_item_${index}`}>
+                <div className="col-12 p-0 pe-3" key={`content_item_${index}`}>
                   <div className="card-body my-3 sec-color">
                   <Link to={`/${data.node.slug}/`} className='linkcard'>
                     <h2 className="card-title">{data.node.H1}</h2>
@@ -69,9 +223,69 @@ const IndexPage = () => {
             })}
             <div className="col-12 col-md-6 col-lg-4 p-0 card-holder" key="added"></div>
 
+
+                </div>
+              </div>
+
+
+          
+
+            <div className="col-8">
+                <div className="row">
+                  <div className="row pb-5 justify-content-end">
+                      <div className="col-11 cat-title-name">
+                        Popular Articles
+                      </div>
+                    </div>
+
+
+
+                  {data.allBlogDataJson.edges.slice(0,7).map((data, index) => {
+                    var excerpto = data.node.excerpt.substring(0,100).split(' ')
+                    
+                    return (    
+                      <>
+                      <Link to={`/${data.node.slug}/`} className='linkcard'>
+                      <div className="row pb-3 justify-content-end" key={`content_item_${index}`}>
+                        <div className="col-3">
+                          <img class="" src={data.node.IMAGE_URL}  width="100%" height="100%" alt="Card image cap"/>
+
+                        </div>
+                        <div className="col-8 px-2">
+
+                            <div className="card-body ps-0 sec-color py-1">
+                            <h2 className="card-big-title">{data.node.H1}</h2>
+                            <p className="card-text" id="cardo">{excerpto.slice(0, excerpto.length - 1).join(' ')} ...</p>
+                            
+                            <div  className="card-link">Read More →</div>
+                            
+                          
+                          </div>
+                        </div>
+                      </div>
+                      </Link>
+                      </>
+                    );
+                  })}
+
+
+                </div>
+              </div>
+
+              </div>
+
+
+
+
+
+
           </div>
+
         </div>
       </Layout>
+      <script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     </div>
   );
 };
